31867bf7df6942b5cdf31420a205951f00f3928f
eugene <medov-foto@mail.ru>
2023-10-05 15:33:51 +0300

    исправил список выполненных, теперь добавляет если в списке еще нет

--- my-laravel-app/public/script.js
+++ my-laravel-app/public/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function (){
     const serviceDivs = document.querySelectorAll('.service')
     const selectedServices = []
+    const addedService = []
     const displaySelectedWorks = document.getElementById('displaySelectedWorks')
 
     serviceDivs.forEach(function (serviceDiv){
@@ -15,22 +16,37 @@ document.addEventListener('DOMContentLoaded', function (){
                 selectedServices.splice(selectedServices.indexOf(serviceId), 1)
                 this.style.background = ''
             }
-            console.log(selectedServices)
+            console.log('selectedService',selectedServices)
         })
     })
 
     const addButton = document.getElementById('addToServiceList')
 
+    function removeHighlight() {
+        serviceDivs.forEach(function (serviceDiv){
+            serviceDiv.style.background = ''
+        })
+    }
+    function updateAddedServiceList() {
+        displaySelectedWorks.innerHTML = ''
+        addedService.forEach(function (serviceId){
+            const serviceDiv = document.createElement('div');
+            serviceDiv.textContent = serviceId
+            displaySelectedWorks.appendChild(serviceDiv)
+        })
+    }
+
     addButton.addEventListener('click', function (){
         selectedServices.forEach(function (serviceId){
-            const serviceDiv = document.createElement('div')
-            serviceDiv.textContent = serviceId;
-
-            const serviceElement = document.querySelector(`[data-id="${serviceId}"]`)
-            serviceElement.style.background = ''
-            displaySelectedWorks.appendChild(serviceDiv);
+            //проверяем наличие такого id
+            if (!addedService.includes(serviceId)) {
+                addedService.push(serviceId)
+            }
         })
-        selectedServices.length = 0;
+        selectedServices.length = 0
+        removeHighlight()
+        updateAddedServiceList()
+        console.log('addedService:',addedService)
     })
 })
 
