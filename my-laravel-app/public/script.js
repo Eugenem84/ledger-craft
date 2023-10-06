faa71a5630e65fafacb8b50eb6e80cdc9a7b862a
eugene <medov-foto@mail.ru>
2023-10-06 10:13:56 +0300

    добавил режим удаления записей из выбранных заказов, но сломалась общая сумма

--- my-laravel-app/public/script.js
+++ my-laravel-app/public/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function (){
     const serviceDivs = document.querySelectorAll('.service')
     const selectedServices = []
-    const addedService = []
+    let addedService = []
     const displaySelectedWorks = document.getElementById('displaySelectedWorks')
 
     console.log(services)
@@ -33,17 +33,39 @@ document.addEventListener('DOMContentLoaded', function (){
         displaySelectedWorks.innerHTML = ''
         let total = 0
         addedService.forEach(function (serviceId){
-            const serviceCh = services.find(service => service.id ===serviceId )
+            const serviceCh = services.find(service => service.id === serviceId )
             if (serviceCh) {
                 const serviceDiv = document.createElement('div');
-                //serviceDiv.textContent = serviceCh.service;\
                 serviceDiv.textContent = `${serviceCh.service} - ${serviceCh.price}`
+                serviceDiv.classList.add('selectable')
+                serviceDiv.dataset.id = serviceCh.id
+
+                const deleteButton = document.createElement('button')
+                deleteButton.textContent = 'удалить'
+                deleteButton.addEventListener('click', function (){
+                    const idToRemove = this.parentElement.dataset.id
+                    addedService = addedService.filter(id => id !==idToRemove)
+                    this.parentElement.remove()
+                    updateTotalPrice()
+                })
+                serviceDiv.appendChild(deleteButton)
+
                 displaySelectedWorks.appendChild(serviceDiv)
                 total += parseInt(serviceCh.price)
             }
         })
         const totalPrice = document.getElementById('totalDiv');
         totalPrice.textContent = total
+
+        const selectableDivs = document.querySelectorAll('.selectable')
+        selectableDivs.forEach(function (div){
+            div.addEventListener('click', function (){
+                selectableDivs.forEach(function (div){
+                    div.classList.remove('selected')
+                })
+                this.classList.toggle('selected')
+            })
+        })
     }
 
     function calculateTotalPrice(){
