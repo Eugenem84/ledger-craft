84f9922f1662f693e5222617327d48db6b261b80
eugene <medov-foto@mail.ru>
2023-11-26 20:32:07 +0300

    прикрутил бутстрап

--- my-laravel-app/resources/views/components/layout.blade.php
+++ my-laravel-app/resources/views/components/layout.blade.php
@@ -3,8 +3,10 @@
 <html>
     <head>
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <title>{{$title}}</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </head>
     <body>
         <header>
@@ -21,5 +23,6 @@
             </ul>
         </nav>
         {{$slot}}
+
     </body>
 </html>
