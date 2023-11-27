6edd9024afc15fd44cf3f8fbb7a2fcbb66c63ccd
eugene <medov-foto@mail.ru>
2023-11-27 12:14:22 +0300

    сделал меню в бутстрап

--- my-laravel-app/resources/views/components/layout.blade.php
+++ my-laravel-app/resources/views/components/layout.blade.php
@@ -2,27 +2,37 @@
 
 <html>
     <head>
-        <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+{{--        <link href="{{ asset('css/styles.css') }}" rel="stylesheet">--}}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <title>{{$title}}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </head>
     <body>
         <header>
-            <div class="header">
-                РЕМОНТ
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h1 class="text-black"> Р Е М О Н Т</h1>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-12">
+                        <a href="/service/show" class="btn btn-itd btn-lg text-uppercase">заказ-наряд</a>
+                        <a href="/service/edit" class="btn btn-itd btn-lg text-uppercase">редактор каталога</a>
+                        <a href="/service/history" class="btn btn-itd btn-lg text-uppercase">история заказов</a>
+                        <a href="/service/statistic" class="btn btn-itd btn-lg text-uppercase">статистика</a>
+
+                    </div>
+                </div>
+
             </div>
         </header>
-        <nav>
-            <ul>
-                <li><a href="/service/show">заказ наряд</a> </li>
-                <li><a href="/service/edit">редактор каталога</a> </li>
-                <li><a href="/service/history">история заказов</a> </li>
-                <li><a href="/service/statistic">статистика</a> </li>
-            </ul>
-        </nav>
         {{$slot}}
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     </body>
 </html>
